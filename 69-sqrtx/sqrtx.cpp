d209c67ca471466b1acb4638cdd25481c3639876
Vaishnavi R Kannur <kannurvaishnavi@gmail.com>
2026-07-06 23:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (87.00%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+      long long low = 0;
+        long long high = x;
+        long long ans = 0;
+
+        while (low <= high) {
+
+            long long mid = low + (high - low) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;   
+    }
+};
