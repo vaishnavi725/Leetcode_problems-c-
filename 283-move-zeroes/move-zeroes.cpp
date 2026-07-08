9e29922e67c70e19bd93bc2e289924f9237895f8
Vaishnavi R Kannur <kannurvaishnavi@gmail.com>
2026-07-08 11:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (5.07%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        while (ans.size() < n) {
+            ans.push_back(0);
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = ans[i];
+        }
+    }
+};
